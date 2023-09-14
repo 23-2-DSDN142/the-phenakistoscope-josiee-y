ec1285d60801617e2720f82b896d2ffae4db88ca
josiee-y <ysyjwq@gmail.com>
2023-09-14 13:26:24 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -3,9 +3,9 @@ const SLICE_COUNT = 10;
 function setup_pScope(pScope){
   //pScope.output_mode(STATIC_FRAME);
   //pScope.output_mode( ANIMATED_FRAME);
-//pScope.output_mode(ANIMATED_DISK);
+pScope.output_mode(ANIMATED_DISK);
 //pScope.output_mode(STATIC_DISK);
-  pScope.output_mode(OUTPUT_GIF(1000));
+  //pScope.output_mode(OUTPUT_PRINT(A3));
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CW);
@@ -63,7 +63,7 @@ function spark(x, y, animation, pScope){
   scale (0.6*animation. frame);
   rotate (360*animation. frame);
 pScope.draw_image("spark",x,y);
-//scale (0.2);
+
 }
 
 function spark1(x, y, animation, pScope){
@@ -72,7 +72,7 @@ function spark1(x, y, animation, pScope){
   rotate (360*animation. frame);
   translate (x,y-400)
 pScope.draw_image("spark",x,y);
-//scale (0.2);
+
 }
 
 
@@ -83,32 +83,7 @@ function magic_wand3(x, y, animation, pScope){
 pScope.draw_image_from_sequence("magic_wand3",0,0, animation.frame);
 }
 
-// function sprinkle(x, y, animation, pScope){
 
-//   let angleOffset = (360 / SLICE_COUNT) / 2
-//   let backgroundArcStart = 270 - angleOffset;
-//   let backgroundArcEnd = 270 + angleOffset;
-
-//   fill(18, 8, 41)
-//   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd);
-
-//   fill(226, 206, 240);
-//   ellipse(-10,-300-animation.wave()*50,20,20);
-
-// }
-
-
-// function faces(x, y, animation, pScope){
-  
-//   scale(animation.frame*2);
-
-//   ellipse(0,0,50,50); // draw head
-//   fill(30);
-//   ellipse(-10,-10,10,10); //draw eye
-//   ellipse(10,-10,10,10); // draw eye
-//   arc(0,10,20,10,0,180); // draw mouth
-
-// }
 
 
 function sunmoon_spin2(x, y, animation, pScope){
@@ -116,7 +91,7 @@ function sunmoon_spin2(x, y, animation, pScope){
   rotate (animation. frame);
   pScope.draw_image_from_sequence("sunmoon_spin2",0,0, animation.frame);
 
-//scale (0.2);
+
 }
 
 
@@ -128,20 +103,15 @@ function squares(x, y, animation, pScope){
   let backgroundArcEnd = 270 + angleOffset;
 push()
   scale (2.5)
-  //rotate (animation.frame);
 pScope.draw_image("sunmoon",x,y);
 pop()
-  // fill(198, 147, 19)
-  // rotate (0*animation. frame);
-  //  arc(x,y,350,350,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  
   
 
   fill(125, 134, 173)
   scale (1*animation. wave(1.5));
   ellipse(100,-500,50,50)
-  //ellipse(-30,-380-animation.wave(0.3)*400,30,30) // .wave is a cosine wave btw
-  //ellipse(70,-500-animation.wave(0.5)*350,30,30) 
-
+  
  
 
 }
@@ -156,17 +126,9 @@ function moon1(x, y, animation, pScope){
 
 
 function spiral4(x, y, animation, pScope){
-  //translate (x,y-840);
   scale (1.5);
   rotate (animation. frame);
   pScope.draw_image("spiral4",x,y);
 
 }
 
-
-
-// function centre (x, y, animation, pScope){
-//   fill(198, 147, 19)
-//   ellipse (0,0,100,100, animation.frame)
-
-// }
